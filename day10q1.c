ab0086f3fc5290109de77313f08d3b67e2eab7a4
hiiimaanshiiii <jindalhimanshi818@gmail.com>
2026-09-02 22:30:20 +0530

    Implement triangle classification based on sides

---
+++ day10q1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float a, b, c;
+    scanf("%f %f %f", &a, &b, &c);
+
+    if (a == b && b == c) {
+        printf("Equilateral\n");
+    } else if (a == b || b == c || a == c) {
+        printf("Isosceles\n");
+    } else {
+        printf("Scalene\n");
+    }
+
+    return 0;
+}
