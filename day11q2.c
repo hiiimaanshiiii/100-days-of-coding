1adac366fd9357b985dd7f623334b56548033853
hiiimaanshiiii <jindalhimanshi818@gmail.com>
2026-09-04 21:42:50 +0530

    Implement profit and loss calculation

---
+++ day11q2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float cp, sp, percentage;
+    scanf("%f %f", &cp, &sp);
+
+    if (sp > cp) {
+        percentage = ((sp - cp) / cp) * 100;
+        printf("Profit %.0f%%", percentage);
+    } else if (sp < cp) {
+        percentage = ((cp - sp) / cp) * 100;
+        printf("Loss %.0f%%", percentage);
+    } else {
+        printf("No Profit No Loss");
+    }
+
+    return 0;
+}
